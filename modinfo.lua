7d4cf478ae899d68e0ff73a7ea6826923ba7bf2d
abma <spring@abma.de>
2015-09-01 01:41:01 +0200

    fix modinfo

--- modinfo.lua
+++ modinfo.lua
@@ -1,11 +1,11 @@
 --http://springrts.com/wiki/Modinfo.lua
 local modinfo = {
-	name = "Dev Test Game",
-	shortname = "DTG",
-	game = "DTG",
-	shortgame = "DTG",
+	name = "devgame",
+	shortname = "dg",
+	game = "dg",
+	shortgame = "dg",
 	description = "Basic game to help engine devs testing stuff",
-	url = "https://github.com/spring/springdevgame",
+	url = "https://github.com/spring/devgame.sdd",
 	version = "$VERSION", --when zipping .sdz for releasing make this a full integer like 1,2,3
 	modtype = 1,
 	depend = {
