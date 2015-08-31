ef58bcefdf7b3557ea847b4d44528d152f256ab3
abma <spring@abma.de>
2015-09-01 01:13:16 +0200

    fix version

--- modinfo.lua
+++ modinfo.lua
@@ -1,12 +1,12 @@
 --http://springrts.com/wiki/Modinfo.lua
 local modinfo = {
-	name = "Spring Tutorial Game",
-	shortname = "STG",
-	game = "STG",
-	shortgame = "STG",
-	description = "Basic game to help new modders",
-	url = "code.google.com/p/springtutorialgame",
-	version = "0.1", --when zipping .sdz for releasing make this a full integer like 1,2,3
+	name = "Dev Test Game",
+	shortname = "DTG",
+	game = "DTG",
+	shortgame = "DTG",
+	description = "Basic game to help engine devs testing stuff",
+	url = "https://github.com/spring/springdevgame",
+	version = "$VERSION", --when zipping .sdz for releasing make this a full integer like 1,2,3
 	modtype = 1,
 	depend = {
 		"cursors.sdz",
