7bff9f41d47c985e431ab2569d4708bcfdaa427c
abma <spring@abma.de>
2016-03-03 23:50:20 +0100

    seems to basicly work now

--- luarules/main.lua
+++ luarules/main.lua
@@ -5,12 +5,21 @@ local TESTS_DIR = Script.GetName():gsub('US$', '') .. '/tests'
 local gadgetFiles = VFS.DirList(TESTS_DIR, "*.lua", VFSMODE)
 local CONFIGVAR = "CurrentTest"
 local curtest = Spring.GetConfigString(CONFIGVAR)
-
+local curgadget = nil
 
 function gadgetHandler:StartTest(testfile)
+	curtest = testfile
 	Spring.Echo("Starting test " .. testfile)
 	Spring.SetConfigString(CONFIGVAR, testfile)
-	self:LoadGadget(testfile)
+	curgadget = gadgetHandler:LoadGadget(testfile)
+	if not (curgadget) then
+		Spring.Echo("error in test: " .. testfile)
+		return
+	end
+	curgadget.TestDone = function (result, msg) self:TestDone(result, msg)      end
+	Spring.Echo("StartTest: Loaded Gadget")
+	gadgetHandler:InsertGadget(curgadget)
+	gadgetHandler:UpdateCallIns()
 end
 
 function gadgetHandler:NextTest()
@@ -31,13 +40,18 @@ function gadgetHandler:NextTest()
 end
 
 function gadgetHandler:TestDone(result, msg)
-	Spring.Echo("Test " .. curtest .. " is done: " .. result .. msg)
+	if result then
+		status = "Ok"
+	else
+		status = "Failed"
+	end
+	Spring.Echo("XXXXXXXXXXXXX Test " .. curtest .. " is done: " .. status .. " ".. msg)
+--gadget.ghInfo.name
+	gadgetHandler:DisableGadget(curgadget.ghInfo.name)
 	gadgetHandler:NextTest()
 end
 
--- how to register gadgetHandler:TestDone() so it can be called from gadgets?
-
-System.TestDone = function (_) self:TestDone(gadget)      end
+--System.TestDone = function (_) gadgetHandler:TestDone(gadget)      end
 
 if curtest == "" then
 	gadgetHandler:NextTest(curtest)
