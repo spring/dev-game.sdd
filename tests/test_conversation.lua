588a70fc7bf4ed2fa4842cb6f7cb0b88fec31843
abma <spring@abma.de>
2016-09-28 23:08:20 +0200

    add lua number conversation test: https://springrts.com/mantis/view.php?id=5358

---
+++ tests/test_conversation.lua
@@ -0,0 +1,29 @@
+function gadget:GetInfo()
+	return {
+		name      = "Test Float conversations",
+		desc      = "number conversation test gadget",
+		author    = "abma",
+		date      = "Sep. 2016",
+		license   = "GNU GPL, v2 or later",
+		layer     = 0,
+		enabled   = true  --  loaded by default?
+	}
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+-- synced only
+if (not gadgetHandler:IsSyncedCode()) then
+	return false
+end
+function gadget:Initialize()
+
+	assert(tonumber("5.1") == 5.1)
+	assert(tonumber("5,1") == nil)
+
+	Spring.Echo(string.format("%f", 5.1))
+	assert(string.format("%f", 5.1) == "5.0999999")
+
+	gadget:TestDone(true, "ok")
+end
